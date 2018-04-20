2c4f7cc6d19187d77e61405d555d3d83a99bbd18
WillyHong <egg358358@gmail.com>
2018-04-21 01:34:59 +0800

    Init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Parcel
+
+Parcel + Vue 新手包
+
+`npm install`
+
+`npm start`
+
+`npm run build`
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+      <h1><p id='text'>{{text}}</p></h1>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      text: 'Hello World !',
+    }
+  },
+  watch:{
+  }
+}
+</script>
+<style lang="scss" scoped>
+h1{
+  color: royalblue;
+}
+</style>
